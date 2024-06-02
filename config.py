2c99c22480be9ec69b5f1b8091a3ec271c2ca7d0
sp77f <spyf77@gmail.com>
2024-06-02 22:41:16 +0300

    del keys

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Telegram Bot API Token
-TOKEN = "6882344636:AAEtVX-q9K3ncGwAU1YR4n_oMOfFWpdMxck"
+TOKEN = "your_token"
 # OpenAI API Key
-api_key1="sk-eojihWMYuwlwO4oNjNMX8DbkkkBtLg7I",
+api_key1="your_api_key",
